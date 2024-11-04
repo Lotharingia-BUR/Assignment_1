95045cac4a7edc27bcc3625001b479921e016aa0
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2024-11-04 12:22:18 -0500

    Assign each asteroid variables

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -7,6 +7,7 @@ public class Asteroid : MonoBehaviour
     public float moveSpeed;
     public float arrivalDistance;
     public float maxFloatDistance;
+    public Transform player;
 
     // Start is called before the first frame update
     void Start()

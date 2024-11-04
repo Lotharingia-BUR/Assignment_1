c293e097b0d34ed79cd735586b8ad4f46c233dda
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2024-11-04 14:02:24 -0500

    Running into issues with creating the List

--- Assets/Scripts/Controllers/Homing_Bombs.cs
+++ Assets/Scripts/Controllers/Homing_Bombs.cs
@@ -6,13 +6,19 @@ using UnityEngine.EventSystems;
 public class Homing_Bombs : MonoBehaviour
 {
     Vector3 moveDirection = Vector3.up;
+    public GameObject apple;
     bool lockOn = false;
-    public List<GameObject> Objects;
+    public List<GameObject> objects;
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < Objects.Capacity; i++)
+        if (!lockOn)
+        {
+            apple = GameObject.Find("Bombs");
+            new List<Vector3> apple.transform.position;
+        }
+        for (int i = 0; i < objects.Capacity; i++)
         {
             Debug.Log(i);
         }
